a6e933726ec1453f31d3f50794ba3f5472affa11
GaoFei <strivek@163.com>
2015-01-26 10:03:27 +0800

    js压缩

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,17 @@
-demo
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+    grunt.initConfig({
+        uglify: {
+            js: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/js',
+                    src: '{*,*/*}.js',
+                    dest: 'dest/js'
+                }]
+            }
+        }
+    });
+    grunt.registerTask("default", ['uglify:js']);
+}
